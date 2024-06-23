3c9d5faa44ce45f0877c120aa195bed52d39f0ed
AnanthuTD <ananthutd2021@gmail.com>
2024-06-23 18:29:42 +0530

    profile update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,47 +1,23 @@
 import express from "express";
 import { hashPassword } from "../helpers/authHelpers.js";
 import { createUser } from "../helpers/userHelpers.js";
+import { updateProfile } from "../helpers/commonHelpers.js";
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-   const userData = req.body;
-
-   console.log(userData);
-
-   if (!(userData.email && userData.password)) {
-      res.json({ error: true, msg: "Email and password are required!" });
-      return;
-   }
-
-   let hashedPassword = undefined;
-
+router.put("/profile", async (req, res) => {
+   const profile = req.body;
+   const user = req.session.user;
    try {
-      hashedPassword = hashPassword({ password: userData.password });
+      const updatedUser = await updateProfile({ profile, _id: user._id });
+      res.json({
+         error: false,
+         msg: "Profile updated successfully",
+         profile: updatedUser,
+      });
    } catch (error) {
+      res.status(500).json({ error: true, msg: error.message });
       console.error(error);
-      res.json({ error: true, msg: "Failed to has password!" });
-      return;
    }
-
-   const newUser = {
-      name:{firstName: userData.firstName, lastName: userData.lastName},
-      email: userData.email,
-      password: hashedPassword,
-   };
-
-   try {
-      await createUser(newUser);
-   } catch (error) {
-      if (error.code === 11000)
-         res.json({ error: true, msg: "User with this email already exist!" });
-      else {
-         res.json({ error: true, msg: "Failed to create user!" });
-         console.error(error);
-      }
-      console.error(error);
-      return;
-   }
-   res.json({ error: false, msg: "User created successfully!" });
 });
 
 export default router;
